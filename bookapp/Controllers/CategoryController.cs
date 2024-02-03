3955a5bd445c2b00c97c55109cda1702a2bfe51a
Bibhot7 <bibhot7@outlook.com>
2024-02-04 07:50:26 +1300

    Update 2, Category Page created and linked with the database.

---
+++ bookapp/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using bookapp.Data;
+using Microsoft.AspNetCore.Mvc;
+using bookapp.Models;
+
+namespace bookapp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        //Getting the implementation of ApplicationDbContext.
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+                _db = db;
+        }
+        public IActionResult Index()
+        {
+            //Retriving all the categories.
+            List <Category> objCategoryList = _db.Categories.ToList();
+            //Passing the category in the view.
+            return View(objCategoryList);
+        }
+    }
+}
--- bookapp/Data/ApplicationDbContext.cs
+++ bookapp/Data/ApplicationDbContext.cs
@@ -16,7 +16,18 @@ namespace bookapp.Data
         {
             
         }
-        public DbSet<Category> categories { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        //Seeding data using Entity framework.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", Display0rder = 1 },
+                new Category { Id = 2, Name = "SciFi", Display0rder = 2 },
+                new Category { Id = 3, Name = "History", Display0rder = 3 }
+
+                );
+        }
 
 
     }
